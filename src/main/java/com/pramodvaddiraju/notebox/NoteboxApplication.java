3202b1321592e582b04fc732916315a45d8431bf
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-04 17:45:10 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/notebox/NoteboxApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.notebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/notebox/NoteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.notebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
